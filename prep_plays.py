94fcb632e1e0c185072bfacc81be6cb52266c792
Angel Gomez <gomez.i.angel3@gmail.com>
2020-12-09 14:38:45 -0600

    update

--- prep_plays.py
+++ prep_plays.py
@@ -12,7 +12,8 @@ def prep_plays_data():
     # keep only the useful columns for mvp
     df = df[['playDescription', 'quarter', 'down', 'yardsToGo', 'possessionTeam',
              'offenseFormation', 'personnelO', 'defendersInTheBox', 'numberOfPassRushers', 
-             'personnelD', 'typeDropback', 'gameClock', 'absoluteYardlineNumber', 'epa', 'playType', 'passResult', 'playResult']]
+             'personnelD', 'typeDropback', 'gameClock', 'absoluteYardlineNumber', 'epa',
+             'playType', 'passResult', 'playResult']]
     # filter out any data that is not a pass play
     df = df[df.playType == 'play_type_pass']
     # creates 0 or 1 for tradtional and scramble
@@ -20,12 +21,13 @@ def prep_plays_data():
                                  'SCRAMBLE':1,'DESIGNED_ROLLOUT_RIGHT':0,
                                  'SCRAMBLE_ROLLOUT_LEFT':1,'DESIGNED_ROLLOUT_LEFT':0,
                                  'UNKNOWN':0}, inplace=True)
-
+    # ranking the teams with the most cumulative passing yards
     df['possessionTeam'].replace({'PIT':2,'GB':9,'TB':1,'IND':6,'ATL':4,'PHI':7,'NYG':11,'DET':20,
                                 'CLE':14,'MIN':13,'LA':5,'OAK':18,'NE':8,'BAL':22,'CAR':32,
                                 'DAL':23,'CIN':24,'DEN':19,'JAX':26,'KC':3,'HOU':17,'WAS':28,
                                 'SF':15,'NYJ':25,'ARI':16,'NO':12,'BUF':31,'CHI':21,'MIA':30,
                                 'LAC':10,'TEN':29,'SEA':27}, inplace=True)
+    # cleaning up the pass result column to only pass complete and pass incomplete
     df['passResult'].replace({'C': 0,'I' : 1, 'IN' : 1}, inplace=True)
     # create a new column that extracts 
     # "(number) RB, (number) TE, (number) WR"
@@ -57,8 +59,6 @@ def prep_plays_data():
     df['LB'] = temp[1].str.replace(r' LB', '')
     # create a new column with the number of DB on the field
     df['DB'] = temp[2].str.replace(r' DB', '')
-    # create dummies for team names
-    teams = pd.get_dummies(df.possessionTeam)
     # create dummies for offensive formation
     formation = pd.get_dummies(df.offenseFormation)
     # Classifying traditional and rollouts into normal dropbacks
@@ -72,10 +72,17 @@ def prep_plays_data():
     df.typeDropback = df.typeDropback.apply(lambda value : str(value).replace('scramble_ROLLOUT_LEFT', 'scramble'))   
     df = df.rename(columns = {'typeDropback' : 'QB_under_pressure', 'passResult' : 'pass_stopped'})
     # join all dataframes together
-    df = pd.concat([df, teams, formation], axis = 1)
+    df = pd.concat([df, formation], axis = 1)
     # drop temporary columns and duplicates
-    df = df.drop(columns = {'personnelO', 'personnelD', 'tempO', 'tempD', 'playType', 'possessionTeam', 'offenseFormation'})
+    df = df.drop(columns = {'personnelO', 'personnelD', 'tempO', 'tempD', 'playType', 'offenseFormation'})
     # reorder the index and drop the old index
+    # Changing datatype from object to int
+    df = df.astype({'DL':'int', 'LB':'int','DB':'int'})
+    # crreating formation columns
+    df['four_three'] = np.where((df['DL'] == 4) & (df['LB'] == 3),1,0)
+    df['three_four'] = np.where((df['DL'] == 3) & (df['LB'] == 4),1,0)
+    df['nickel'] = np.where(df['DB'] == 5, 1, 0)
+    df['dime'] = np.where(df['DB'] == 6, 1, 0)
     df = df.reset_index(drop=True)
     df = df.dropna()
     return df
